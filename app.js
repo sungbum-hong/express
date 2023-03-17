cd4b74baed23c92f613d42776e727c06beb5b62c
Sungbeom <mkonji5466@gmail.com>
2023-03-17 15:44:51 +0900

    20230317 회원가입 로그인

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const PORT = 4000;
@@ -11,6 +12,16 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: 'sb',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
 // 라우터 (파일) //
 const mainRouter = require('./routes'); //./router/index.js 생략
@@ -19,6 +30,8 @@ const boardRouter = require('./routes/board');
 const dbRouter = require('./routes/db');
 const dbBoardRouter = require('./routes/dbBoard');
 const cookieRouter = require('./routes/cookie');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
 
 // 사용 '/파일.js//
 app.use('/', mainRouter);
@@ -27,6 +40,8 @@ app.use('/board', boardRouter);
 app.use('/db', dbRouter);
 app.use('/dbBoard', dbBoardRouter);
 app.use('/cookie', cookieRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 //
 app.use((err, req, res, next) => {
--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -1,12 +1,26 @@
 const connection = require('./db.Contenct');
 
 const userDB = {
-  getUsers: (cb) => {
-    connection.query('SELECT * FROM mydb.user;', (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      cb(data);
-    });
+  // 중복 회원 찾기
+  userCheck: (userId, cb) => {
+    connection.query(
+      `SELECT * FROM mydb.user WHERE USERID = '${userId}';`,
+      (err, data) => {
+        if (err) throw err;
+        console.log(data);
+        cb(data);
+      }
+    );
+  },
+  // 회원 가입 하기
+  registerUser: (newUser, cb) => {
+    connection.query(
+      `INSERT INTO mydb.user (USERID, PASSWORD) values ('${newUser.id}','${newUser.password}');`,
+      (err, data) => {
+        if (err) throw err;
+        cb(data);
+      }
+    );
   },
 };
 
